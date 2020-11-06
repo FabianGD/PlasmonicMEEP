48f9205bed6cfde550829b4da5579747beb49c85
Fabian George Dröge <fabian.g.droege@gmail.com>
2020-11-06 16:02:41 +0100

    Worked towards understanding the bhmie code.

--- src/bhmie/bhmie.py
+++ src/bhmie/bhmie.py
@@ -6,6 +6,9 @@ def bhmie(x, refrel, n_angles):
     This file is converted from mie.m, see http://atol.ucsd.edu/scatlib/index.htm
     Bohren and Huffman originally published the code in their book on light scattering
 
+    Literature:
+    - https://doi.org/10.1364/AO.43.001951
+
     Calculation based on Mie scattering theory
     input:
          x      - size parameter = k*radius = 2pi/lambda * radius
@@ -37,17 +40,17 @@ def bhmie(x, refrel, n_angles):
     if n_angles < 2:
         n_angles = 2
 
-    dx = x
-
-    drefrl = refrel
-    y = x * drefrl
+    y = x * refrel
+    # ymod = abs(m * x)
     ymod = np.abs(y)
 
-    #    Series expansion terminated after NSTOP terms
-    #    Logarithmic derivatives calculated from NMX on down
+    # Series expansion terminated after NSTOP terms
+    # Logarithmic derivatives calculated from NMX on down
+    # Definition of the break criterion
+    criterion_flt = x + 4.0 * x ** 0.3333 + 2.0
+    nr_expansion = int(criterion_flt)
 
-    xstop = x + 4.0 * x ** 0.3333 + 2.0
-    nmx = np.max([xstop, ymod]) + 15.0
+    nmx = np.max([criterion_flt, ymod]) + 15.0
     nmx = np.fix(nmx)
 
     # BTD experiment 91/1/15: add one more term to series and compare results
@@ -57,122 +60,135 @@ def bhmie(x, refrel, n_angles):
     # computed number changed (out of 4*7001) and it only changed by 1/8387
     # conclusion: we are indeed retaining enough terms in series!
 
-    nstop = int(xstop)
-
     if nmx > nmxx:
         print("error: nmx > nmxx=%f for |m|x=%f" % (nmxx, ymod))
         return
 
     delta_angle = 0.5 * np.pi / (n_angles - 1)
+    thetas = np.arange(n_angles, dtype=np.float64) * delta_angle
+    amu = np.cos(thetas)
 
-    amu = np.arange(0.0, n_angles, 1)
-    amu = np.cos(amu * delta_angle)
+    pi_0 = np.zeros(n_angles)
+    pi_1 = np.ones(n_angles)
 
-    pi0 = np.zeros(n_angles)
-    pi1 = np.ones(n_angles)
-
-    # Logarithmic derivative D(J) calculated by downward recurrence
+    # Logarithmic derivative D_n(J) calculated by downward recurrence
     # beginning with initial value (0.,0.) at J=NMX
 
-    nn = int(nmx) - 1
-    d = np.zeros(nn + 1)
-    for n in range(0, nn):
-        en = nmx - n
-        d[nn - n - 1] = (en / y) - (1.0 / (d[nn - n] + en / y))
+    max_order = int(nmx) - 1
+    logderiv_d = np.zeros(max_order + 1)
+    for idx in range(max_order):
+        # Assign the order and the index
+        order = nmx - idx
+        order_idx = max_order - idx
+
+        # Actually compute the log derivate.
+        logderiv_d[order_idx - 1] = (order / y) - (
+            1.0 / (logderiv_d[order_idx] + order / y)
+        )
 
     # *** Riccati-Bessel functions with real argument X
     #    calculated by upward recurrence
 
-    psi0 = np.cos(dx)
-    psi1 = np.sin(dx)
-    chi0 = -np.sin(dx)
-    chi1 = np.cos(dx)
-    xi1 = psi1 - chi1 * 1j
+    # In the loop, the following holds:
+    # for given N, PSI  = psi_n        CHI  = chi_n
+    #              PSI1 = psi_{n-1}    CHI1 = chi_{n-1}
+    #              PSI0 = psi_{n-2}    CHI0 = chi_{n-2}
+
+    psi_0 = np.cos(x)
+    psi_m1 = np.sin(x)
+    chi_0 = -np.sin(x)
+    chi_m1 = np.cos(x)
+    xi_1 = psi_m1 - chi_m1 * 1j
     qsca = 0.0
     gsca = 0.0
-    p = -1
+    sign = -1
 
-    for n in range(0, nstop):
-        en = n + 1.0
-        fn = (2.0 * en + 1.0) / (en * (en + 1.0))
+    a_n: float = 0.0
+    b_n: float = 0.0
+    a_n1: float = 0.0
+    b_n1: float = 0.0
 
-        # for given N, PSI  = psi_n        CHI  = chi_n
-        #              PSI1 = psi_{n-1}    CHI1 = chi_{n-1}
-        #              PSI0 = psi_{n-2}    CHI0 = chi_{n-2}
+    # This loop computes a Taylor series expansion
+    for n in range(0, nr_expansion):
+        curr_n = n + 1.0
+        factor_n = (2.0 * curr_n + 1.0) / (curr_n * (curr_n + 1.0))
+
         # Calculate psi_n and chi_n
-
-        psi = (2.0 * en - 1.0) * psi1 / dx - psi0
-        chi = (2.0 * en - 1.0) * chi1 / dx - chi0
+        psi = (2.0 * curr_n - 1.0) * psi_m1 / x - psi_0
+        chi = (2.0 * curr_n - 1.0) * chi_m1 / x - chi_0
         xi = psi - chi * 1j
 
         # *** Store previous values of AN and BN for use
         #     in computation of g=<cos(theta)>
         if n > 0:
-            an1 = an
-            bn1 = bn
+            a_n1 = a_n  # type: ignore
+            b_n1 = b_n  # type: ignore
 
         # *** Compute AN and BN:
-        an = (d[n] / drefrl + en / dx) * psi - psi1
-        an = an / ((d[n] / drefrl + en / dx) * xi - xi1)
-        bn = (drefrl * d[n] + en / dx) * psi - psi1
-        bn = bn / ((drefrl * d[n] + en / dx) * xi - xi1)
+        a_n = (logderiv_d[n] / refrel + curr_n / x) * psi - psi_m1
+        a_n /= (logderiv_d[n] / refrel + curr_n / x) * xi - xi_1
+
+        b_n = (refrel * logderiv_d[n] + curr_n / x) * psi - psi_m1
+        b_n /= (refrel * logderiv_d[n] + curr_n / x) * xi - xi_1
 
         # *** Augment sums for Qsca and g=<cos(theta)>
-        qsca += (2.0 * en + 1.0) * (abs(an) ** 2 + abs(bn) ** 2)
-        gsca += ((2.0 * en + 1.0) / (en * (en + 1.0))) * (
-            np.real(an) * np.real(bn) + np.imag(an) * np.imag(bn)
+        qsca += (2.0 * curr_n + 1.0) * (abs(a_n) ** 2 + abs(b_n) ** 2)
+        gsca += ((2.0 * curr_n + 1.0) / (curr_n * (curr_n + 1.0))) * (
+            np.real(a_n) * np.real(b_n) + np.imag(a_n) * np.imag(b_n)
         )
 
         if n > 0:
-            gsca += ((en - 1.0) * (en + 1.0) / en) * (
-                np.real(an1) * np.real(an)
-                + np.imag(an1) * np.imag(an)
-                + np.real(bn1) * np.real(bn)
-                + np.imag(bn1) * np.imag(bn)
+            gsca += ((curr_n - 1.0) * (curr_n + 1.0) / curr_n) * (
+                np.real(a_n1) * np.real(a_n)
+                + np.imag(a_n1) * np.imag(a_n)
+                + np.real(b_n1) * np.real(b_n)
+                + np.imag(b_n1) * np.imag(b_n)
             )
 
         # *** Now calculate scattering intensity pattern
-        #    First do angles from 0 to 90
-        pi = 0 + pi1  # 0+pi1 because we want a hard copy of the values
-        tau = en * amu * pi - (en + 1.0) * pi0
-        s1_1 += fn * (an * pi + bn * tau)
-        s2_1 += fn * (an * tau + bn * pi)
+        #     First do angles from 0 to 90
+        pi = pi_1.copy()  # 0+pi1 because we want a hard copy of the values
+        tau = curr_n * amu * pi - (curr_n + 1.0) * pi_0
+        s1_1 += factor_n * (a_n * pi + b_n * tau)
+        s2_1 += factor_n * (a_n * tau + b_n * pi)
 
         # *** Now do angles greater than 90 using PI and TAU from
-        #    angles less than 90.
-        #    P=1 for N=1,3,...% P=-1 for N=2,4,...
-        #   remember that we have to reverse the order of the elements
-        #   of the second part of s1 and s2 after the calculation
-        p = -p
-        s1_2 += fn * p * (an * pi - bn * tau)
-        s2_2 += fn * p * (bn * pi - an * tau)
+        #     angles less than 90.
+        #     P=1 for N=1,3,...% P=-1 for N=2,4,...
+        #     remember that we have to reverse the order of the elements
+        #     of the second part of s1 and s2 after the calculation
+        sign = -sign
+        s1_2 += factor_n * sign * (a_n * pi - b_n * tau)
+        s2_2 += factor_n * sign * (b_n * pi - a_n * tau)
 
-        psi0 = psi1
-        psi1 = psi
-        chi0 = chi1
-        chi1 = chi
-        xi1 = psi1 - chi1 * 1j
+        psi_0 = psi_m1
+        psi_m1 = psi
+
+        chi_0 = chi_m1
+        chi_m1 = chi
+
+        xi_1 = psi_m1 - chi_m1 * 1j
 
         # *** Compute pi_n for next value of n
-        #    For each angle J, compute pi_n+1
-        #    from PI = pi_n , PI0 = pi_n-1
-        pi1 = ((2.0 * en + 1.0) * amu * pi - (en + 1.0) * pi0) / en
-        pi0 = 0 + pi  # 0+pi because we want a hard copy of the values
+        #     For each angle J, compute pi_n+1
+        #     from PI = pi_n , PI0 = pi_n-1
+        pi_1 = ((2.0 * curr_n + 1.0) * amu * pi - (curr_n + 1.0) * pi_0) / curr_n
+        pi_0 = pi.copy()  # 0+pi because we want a hard copy of the values
 
     # *** Have summed sufficient terms.
-    #    Now compute QSCA,QEXT,QBACK,and GSCA
+    #     Now compute QSCA,QEXT,QBACK,and GSCA
 
-    #   we have to reverse the order of the elements of the second part of s1 and s2
+    # we have to reverse the order of the elements of the second part of s1 and s2
     s1 = np.concatenate((s1_1, s1_2[-2::-1]))
     s2 = np.concatenate((s2_1, s2_2[-2::-1]))
     gsca = 2.0 * gsca / qsca
-    qsca = (2.0 / (dx * dx)) * qsca
-    qext = (4.0 / (dx * dx)) * np.real(s1[0])
+    qsca = (2.0 / x ** 2) * qsca
+    qext = (4.0 / x ** 2) * np.real(s1[0])
 
     # more common definition of the backscattering efficiency,
     # so that the backscattering cross section really
     # has dimension of length squared
-    qback = 4 * (np.abs(s1[2 * n_angles - 2]) / dx) ** 2
+    qback = 4 * (np.abs(s1[2 * n_angles - 2]) / x) ** 2
     # qback = ((abs(s1[2*nang-2])/dx)**2 )/pii  #old form
 
     return s1, s2, qext, qsca, qback, gsca
@@ -180,4 +196,3 @@ def bhmie(x, refrel, n_angles):
 
 if __name__ == "__main__":
     bhmie(x=0.1, refrel=1.5 + 0.5j, n_angles=100)
-
