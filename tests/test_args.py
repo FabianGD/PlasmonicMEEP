b83129115a7f029c28c73823a885c10240d086ca
Fabian George Dröge <fabian.g.droege@gmail.com>
2021-07-02 11:40:18 +0200

    Formatting of the test_args.py

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,23 +1,29 @@
-
-from plasmonicmeep.args import positive_type  # pylint: disable=import-error
 import pytest
 
-@pytest.mark.parametrize("value,rtype,other_allowed,expected", [
-    pytest.param("-10", float, None, None, marks=pytest.mark.xfail),
-    pytest.param("-10", int, None, None, marks=pytest.mark.xfail),
-    pytest.param("0", int, None, None, marks=pytest.mark.xfail),
-    pytest.param("auto", int, None, None, marks=pytest.mark.xfail),
-    pytest.param("1.3", int, None, None, marks=pytest.mark.xfail),
-    pytest.param("-1", int, [-1], -1),
-    pytest.param("-1", int, ["-1"], "-1"),
-    pytest.param("10", int, None, 10),
-    pytest.param("1.3", float, None, 1.3),
-    pytest.param("1E9", float, None, 1E9),
-    pytest.param("1000000", int, None, 1_000_000),
-    pytest.param("auto", int, ["auto"], "auto"),
-])
-def test_positive_type(value, rtype, other_allowed, expected):
+from plasmonicmeep.args import positive_type  # pylint: disable=import-error
 
+
+@pytest.mark.parametrize(
+    "value,rtype,other_allowed,expected",
+    [
+        pytest.param("-10", float, None, None, marks=pytest.mark.xfail),
+        pytest.param("-10", int, None, None, marks=pytest.mark.xfail),
+        pytest.param("0", int, None, None, marks=pytest.mark.xfail),
+        pytest.param("auto", int, None, None, marks=pytest.mark.xfail),
+        pytest.param("1.3", int, None, None, marks=pytest.mark.xfail),
+        pytest.param("-1", int, [-1], -1),
+        pytest.param("-1", int, ["-1"], "-1"),
+        pytest.param("10", int, None, 10),
+        pytest.param("1.3", float, None, 1.3),
+        pytest.param("1E9", float, None, 1e9),
+        pytest.param("1000000", int, None, 1_000_000),
+        pytest.param("auto", int, ["auto"], "auto"),
+    ],
+)
+def test_positive_type(value, rtype, other_allowed, expected):
+    """
+    Test cases for the utils.positive_type() function
+    """
     rval = positive_type(value, rtype, other_allowed)
 
     assert rval == expected
