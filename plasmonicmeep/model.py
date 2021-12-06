e4b84d3e3ba0a39febcc21c20550f98008897db8
Jyothika Pillay <bi87taw@login01.ara>
2021-12-06 22:18:10 +0100

    cleanup: minor formatting

--- plasmonicmeep/model.py
+++ plasmonicmeep/model.py
@@ -66,81 +66,102 @@ def create_sinus_grating(
 
 
 def bow_y(
-    height: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    height: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Vector3]:
-    """
-    Create bowtie-shaped dimer oriented along y axis.
+    """Create bowtie-shaped dimer oriented along y axis.
+
+    Args:
+        height (float): The height of the individual prisms making up the dimer. 
+            Required argument (no default).
+        separation (float): The separation distance between the prisms. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
 
     # Calculate vertices
-    side_length = np.sqrt(height**2/3)
+    side_length = np.sqrt(height ** 2 / 3)
 
     vertices1 = [
-        mp.Vector3(center.x , center.y - separation/2, center.z),
-        mp.Vector3(center.x - side_length, center.y - separation/2 - height, center.z),
-        mp.Vector3(center.x + side_length, center.y - separation/2 - height, center.z)
+        mp.Vector3(center.x, center.y - separation / 2),
+        mp.Vector3(center.x - side_length, center.y - separation / 2 - height),
+        mp.Vector3(center.x + side_length, center.y - separation / 2 - height),
     ]
     vertices2 = [
-        mp.Vector3(center.x, center.y + separation/2, center.z),
-        mp.Vector3(center.x - side_length, center.y + separation/2 + height, center.z),
-        mp.Vector3(center.x + side_length, center.y + separation/2 + height, center.z)
+        mp.Vector3(center.x, center.y + separation / 2),
+        mp.Vector3(center.x - side_length, center.y + separation / 2 + height),
+        mp.Vector3(center.x + side_length, center.y + separation / 2 + height),
     ]
 
-    triangle1 = mp.Prism(vertices1, height = mp.inf, material=material)
-    triangle2 = mp.Prism(vertices2, height = mp.inf, material=material)
+    triangle1 = mp.Prism(vertices1, height=mp.inf, material=material)
+    triangle2 = mp.Prism(vertices2, height=mp.inf, material=material)
 
     return [triangle1, triangle2]
 
 
 def bow_x(
-    height: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    height: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Vector3]:
-    """
-    Create bowtie-shaped dimer oriented along x axis.
+    """Create bowtie-shaped dimer oriented along x axis.
+
+    Args:
+        height (float): The height of the individual prisms making up the dimer. 
+            Required argument (no default).
+        separation (float): The separation distance between the prisms. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
 
     # Calculate vertices
-    side_length = np.sqrt(height**2/3)
+    side_length = np.sqrt(height ** 2 / 3)
 
     vertices1 = [
-        mp.Vector3(center.x - separation/2, center.y, center.z),
-        mp.Vector3(center.x - separation/2 - height, center.y - side_length, center.z),
-        mp.Vector3(center.x - separation/2 - height, center.y + side_length, center.z)
+        mp.Vector3(center.x - separation / 2, center.y),
+        mp.Vector3(center.x - separation / 2 - height, center.y - side_length),
+        mp.Vector3(center.x - separation / 2 - height, center.y + side_length),
     ]
     vertices2 = [
-        mp.Vector3(center.x + separation/2, center.y, center.z),
-        mp.Vector3(center.x + separation/2 + height, center.y - side_length, center.z),
-        mp.Vector3(center.x + separation/2 + height, center.y + side_length, center.z)
+        mp.Vector3(center.x + separation / 2, center.y),
+        mp.Vector3(center.x + separation / 2 + height, center.y - side_length),
+        mp.Vector3(center.x + separation / 2 + height, center.y + side_length),
     ]
 
-    triangle1 = mp.Prism(vertices1, height = mp.inf, material=material)
-    triangle2 = mp.Prism(vertices2, height = mp.inf, material=material)
+    triangle1 = mp.Prism(vertices1, height=mp.inf, material=material)
+    triangle2 = mp.Prism(vertices2, height=mp.inf, material=material)
 
     return [triangle1, triangle2]
 
 
 def spheres_y(
-    radius: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    radius: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Sphere]:
-    """
-    Create dimer of spherical nanoparticles oriented along y axis.
+    """Create dimer of spherical nanoparticles oriented along y axis.
 
+    Args:
+        radius (float): The radius of the individual spheres making up the dimer. 
+            Required argument (no default).
+        separation (float): The separation distance between the spheres. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
+
     # Calculate spherical coordinates
-    sphere1_pos = mp.Vector3(center.x, center.y - radius - separation/2, center.z)
-    sphere2_pos = mp.Vector3(center.x, center.y + radius + separation/2, center.z)
+    sphere1_pos = mp.Vector3(center.x, center.y - radius - separation / 2)
+    sphere2_pos = mp.Vector3(center.x, center.y + radius + separation / 2)
 
     sphere1 = mp.Sphere(center=sphere1_pos, radius=radius, material=material)
     sphere2 = mp.Sphere(center=sphere2_pos, radius=radius, material=material)
@@ -149,83 +170,105 @@ def spheres_y(
 
 
 def spheres_x(
-    radius: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    radius: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Sphere]:
-    """
-    Create dimer of spherical nanoparticles oriented along x axis.
+    """Create dimer of spherical nanoparticles oriented along x axis.
+
+    Args:
+        radius (float): The radius of the individual spheres making up the dimer.  
+            Required argument (no default).
+        separation (float): The separation distance between the spheres. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
 
     # Calculate spherical coordinates
-    sphere1_pos = mp.Vector3(center.x - radius - separation / 2, center.y, center.z)
-    sphere2_pos = mp.Vector3(center.x + radius + separation / 2, center.y, center.z)
+    sphere1_pos = mp.Vector3(center.x - radius - separation / 2, center.y)
+    sphere2_pos = mp.Vector3(center.x + radius + separation / 2, center.y)
 
     sphere1 = mp.Sphere(center=sphere1_pos, radius=radius, material=material)
     sphere2 = mp.Sphere(center=sphere2_pos, radius=radius, material=material)
 
     return [sphere1, sphere2]
 
+
 def invertedtr_y(
-    height: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    height: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Vector3]:
-    """
-    Create dimer of inverted triangular nanoparticles oriented along y axis.
+    """Create dimer of inverted triangular nanoparticles oriented along y axis.
+
+    Args:
+        height (float): The height of the individual prisms making up the dimer. 
+            Required argument (no default).
+        separation (float): The separation distance between the prisms. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
 
     # Calculate vertices
-    side_length = np.sqrt(height**2/3)
+    side_length = np.sqrt(height ** 2 / 3)
 
     vertices1 = [
-        mp.Vector3(center.x, center.y - separation/2 - height, center.z),
-        mp.Vector3(center.x - side_length, center.y - separation/2, center.z),
-        mp.Vector3(center.x + side_length, center.y - separation/2, center.z)
+        mp.Vector3(center.x, center.y - separation / 2 - height),
+        mp.Vector3(center.x - side_length, center.y - separation / 2),
+        mp.Vector3(center.x + side_length, center.y - separation / 2),
     ]
     vertices2 = [
-        mp.Vector3(center.x, center.y + separation/2 + height, center.z),
-        mp.Vector3(center.x - side_length, center.y + separation/2, center.z),
-        mp.Vector3(center.x + side_length, center.y + separation/2, center.z)
+        mp.Vector3(center.x, center.y + separation / 2 + height),
+        mp.Vector3(center.x - side_length, center.y + separation / 2),
+        mp.Vector3(center.x + side_length, center.y + separation / 2),
     ]
 
-    triangle1 = mp.Prism(vertices1, height = mp.inf, material=material)
-    triangle2 = mp.Prism(vertices2, height = mp.inf, material=material)
+    triangle1 = mp.Prism(vertices1, height=mp.inf, material=material)
+    triangle2 = mp.Prism(vertices2, height=mp.inf, material=material)
 
     return [triangle1, triangle2]
 
 
 def invertedtr_x(
-    height: float,
-    separation: float,
-    center: mp.Vector3,
-    material: mp.Medium,
-    y: bool = False,
+    height: float, separation: float, center: mp.Vector3, material: mp.Medium,
 ) -> List[mp.Vector3]:
-    """
-    Create dimer of inverted triangular nanoparticles oriented along x axis.
+    """Create dimer of inverted triangular nanoparticles oriented along x axis.
+
+    Args:
+        height (float): The height of the individual prisms making up the dimer. 
+            Required argument (no default).
+        separation (float): The separation distance between the prisms. 
+            Required argument (no default).
+        center (mp.Vector3): The coordinates of the center of the dimer. 
+            Required argument (no default).
+        material (mp.Medium): The material of the dimer. Required argument (no default).
+
+    Returns:
+        List[mp.Vector3]: List of geometric structures making up the dimer being simulated.
     """
 
     # Create vertices
-    side_length = np.sqrt(height**2/3)
+    side_length = np.sqrt(height ** 2 / 3)
 
     vertices1 = [
-        mp.Vector3(center.x - separation/2 - height, center.y, center.z),
-        mp.Vector3(center.x - separation/2, center.y - side_length, center.z),
-        mp.Vector3(center.x - separation/2, center.y + side_length, center.z)
+        mp.Vector3(center.x - separation / 2 - height, center.y),
+        mp.Vector3(center.x - separation / 2, center.y - side_length),
+        mp.Vector3(center.x - separation / 2, center.y + side_length),
     ]
     vertices2 = [
-        mp.Vector3(center.x + separation/2 + height, center.y, center.z),
-        mp.Vector3(center.x + separation/2, center.y - side_length, center.z),
-        mp.Vector3(center.x + separation/2, center.y + side_length, center.z)
+        mp.Vector3(center.x + separation / 2 + height, center.y),
+        mp.Vector3(center.x + separation / 2, center.y - side_length),
+        mp.Vector3(center.x + separation / 2, center.y + side_length),
     ]
 
-    triangle1 = mp.Prism(vertices1, height = mp.inf, material=material)
-    triangle2 = mp.Prism(vertices2, height = mp.inf, material=material)
+    triangle1 = mp.Prism(vertices1, height=mp.inf, material=material)
+    triangle2 = mp.Prism(vertices2, height=mp.inf, material=material)
 
     return [triangle1, triangle2]
 
@@ -238,3 +281,6 @@ MODEL_MAPPING = {
     "inv-bow-y": invertedtr_y,
     "inv-bow-x": invertedtr_x,
 }
+
+if __name__ == "__main__":
+    bow_y
