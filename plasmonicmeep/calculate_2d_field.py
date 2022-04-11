bbc65730ea3db2906ac543036b14a0007e4fbaf1
Fabian George Dröge <fabian.g.droege@gmail.com>
2022-04-11 09:35:39 +0200

    plas-field: speed up by removing rdcc_nbytes and using direct_read

--- plasmonicmeep/calculate_2d_field.py
+++ plasmonicmeep/calculate_2d_field.py
@@ -8,10 +8,12 @@ import logging
 import h5py
 import numpy as np
 from joblib import Parallel, delayed
+from .plotting import read_h5ds_direct
 
 
 class PlasmonicError(Exception):
     """Custom error to indicate errors in the logic of the program flow."""
+
 
 def argparsing():
     """
@@ -36,7 +38,9 @@ def argparsing():
     return args
 
 
-def get_power(data, interp=4, nproc=4, parallel=True):
+def get_power(
+    data: np.ndarray, interp: int = 4, nproc: int = 4, parallel: bool = True
+) -> np.ndarray:
     """
     Convert a chunk of data from time domain to squared frequency domain (power spectrum)
     """
@@ -74,7 +78,7 @@ def main():
 
     # Read all the data from the data and reference files.
     logging.info("Opening {} as data file...".format(args.file))
-    dfile = h5py.File(args.file, "r", rdcc_nbytes=500 * 1024 ** 2)
+    dfile = h5py.File(args.file, "r")
 
     # Read the cfreq and fwidth from the h5py file.
     cfreq = dfile.attrs["cfreq"]
@@ -85,12 +89,14 @@ def main():
     data = [dfile[key] for key in keys]
 
     # read all datasets in the hf5 file
-    rfile = h5py.File(args.reffile, "r", rdcc_nbytes=500 * 1024 ** 2)
+    rfile = h5py.File(args.reffile, "r")
     reference = [rfile[key] for key in keys]
 
     # Assert that the data matches.
     if not (cfreq == rfile.attrs["cfreq"] or freq_width == rfile.attrs["fwidth"]):
-        raise PlasmonicError("The frequencies of the provided files do not match. Exiting.")
+        raise PlasmonicError(
+            "The frequencies of the provided files do not match. Exiting."
+        )
 
     logging.info("Reference shape: {}".format(reference[0].shape))
     logging.info("Components: {}".format(keys))
@@ -116,7 +122,7 @@ def main():
 
     # Open new output h5 file
     filename = args.savefile
-    outfile = h5py.File(filename, "w", rdcc_nbytes=500 * 1024 ** 2)
+    outfile = h5py.File(filename, "w")
     dset = outfile.create_dataset(
         "enhancement", (x_length, y_length, freq_length), dtype="f"
     )
@@ -129,19 +135,22 @@ def main():
     for i in range(0, x_length, batch_size):
 
         logging.info("Iteration {} from {}".format(i, x_length))
-        bshape = data[0][i : i + batch_size, ...].shape
+        bshape = data[0].shape
 
-        enh_data = np.zeros((bshape[0], bshape[1], freq_length))
-        ref_data = np.zeros((bshape[0], bshape[1], freq_length))
+        enh_data = np.zeros((batch_size, bshape[1], freq_length))
+        ref_data = np.zeros((batch_size, bshape[1], freq_length))
 
         for component_e, component_r in zip(data, reference):
             logging.info("Processing component {} of data.".format(component_e.name))
             logging.info("Processing component {} of ref.".format(component_r.name))
 
-            enh_data += get_power(component_e[i : i + batch_size, ...])
-            ref_data += get_power(component_r[i : i + batch_size, ...])
+            enh_arr = read_h5ds_direct(component_e, np.s_[i : i + batch_size, :, :])
+            ref_arr = read_h5ds_direct(component_r, np.s_[i : i + batch_size, :, :])
 
-        dset[i : i + batch_size, ...] = enh_data[...] / ref_data[...]
+            enh_data += get_power(enh_arr)
+            ref_data += get_power(ref_arr)
+
+        dset[i : i + batch_size, ...] = enh_data / ref_data
 
     # Close all the files.
     dfile.close()
