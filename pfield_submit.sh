3fe18333dad3dcd00aae1d817fb3d38f9163378c
Fabian G. Dröge <fabian.g.droege@gmail.com>
2022-07-25 11:35:50 +0200

    slurm: update submit script for ARA

--- pfield_submit.sh
+++ pfield_submit.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=pyQD
 #SBATCH --partition=s_standard
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=12
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --output=%J.out
-#SBATCH --mem-per-cpu=3GB
+#SBATCH --mem-per-cpu=5GB
 
 # Load modules
 module purge
-module load tools/python/3.8
 
-eval "$(conda shell.bash hook)"
+eval "$(/beegfs/lu27wil/miniconda3/bin/conda shell.bash hook)"
 
 # Load the pyQD conda environment
-conda activate pmeep
+conda activate pmp
 
 set -x
 
@@ -25,7 +24,10 @@ mkdir -p $TMPDIR
 args=( $@ )
 
 # Get the necessary info
-dir=${args[@]:0:1}
-cargs=${args[@]:1}
+dir=$(realpath ${args[@]:0:1})
+echo $dir
 
-plas-field $dir/plas-meep-norm.h5 $dir/plas-meep-ref.h5 $dir/pfield.h5 $cargs
+cargs=${args[@]:1}
+echo $cargs
+
+plas-field $dir/plas-meep-norm.h5 $dir/plas-meep-ref.h5 $dir/pfield.h5 -n 12 $cargs
